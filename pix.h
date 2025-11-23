8585cd8ffbd2b1f6e08f0afb17d69f0ff06915ed
Shahd Mostafa <164599571+Shahd3711@users.noreply.github.com>
2025-11-23 14:37:08 +0200

    pix.h

--- pix.h
+++ pix.h
@@ -10,24 +10,25 @@ extern struct termios orig_termios;
 #define MAX_TRACK_LENGTH 50
 #define MAX_INSTRUCTOR_LENGTH 50
 using namespace std;
-struct Student 
+struct Student
 {
-    char name[MAX_NAME_LENGTH];
-    int age;
-    char track[MAX_TRACK_LENGTH];
-    char instructor[MAX_INSTRUCTOR_LENGTH];
+char name[MAX_NAME_LENGTH];
+int age;
+char track[MAX_TRACK_LENGTH];
+char instructor[MAX_INSTRUCTOR_LENGTH];
 };
 void clearScreen();
 void setColor(const string& colorName);
 void gotoXY(int x, int y);
 pair<int,int> getTerminalSize();
 void printCenter(const char* text, const std::string& color="white");
-int readKey(); 
-void waitForRawEnter(); 
+int readKey();
+void waitForRawEnter();
 void flushInput();
 void enableRawMode();
 void disableRawMode();
 void drawMenu(const string menu[], int menuSize, int selected);
+bool isValidName(const char* name);
 Student getStudentInput();
 void displayStudents(const Student students[], int count);
 #endif
